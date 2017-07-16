c18df6eb1ef37b25ede52b7d033cf34b82f140ce
JunePaloma <kborkoski@gmail.com>
2017-07-16 17:37:14 -0600

    Add additional conditions  to gameboard for big ship

--- test/game_board_test.rb
+++ test/game_board_test.rb
@@ -17,8 +17,8 @@ class GameBoardTest < Minitest::Test
     assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"], @board.empty?
   end
 
-  def test_place_ship
-    assert_equal "Your ship has been placed", @board.place_ship("A1", "A2")
+  def test_place_little_ship
+    assert_equal , @board.place_ship("A1", "A2")
   end
 
 end
